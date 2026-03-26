e86ad3bbeb078c9cbaf0d766c09676d011ec1546
AnastasiaLL <anastasia.lukyanchik@gmail.com>
2026-03-26 13:56:27 +0300

    fix: package.json

--- server/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { WebSocketServer } from 'ws';
-
-
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-
-// WebSocket server
-const wss = new WebSocketServer({ port: PORT });
